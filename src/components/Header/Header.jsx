aab63c6469241ff6d4dd606449b2586138aa7ee9
Sumaiya Kawsar <sumaiya.kawsar693@gmail.com>
2022-09-10 07:37:33 +0300

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,20 +3,20 @@ import Toggle from "./Toggle";
 import { Link } from "react-router-dom";
 const Header = () => {
   return (
-    <div className="fixed w-full top-0 h-16 bg-neutral-50   dark:bg-[#2b3945] shadow-md ">
+    <header className="fixed w-full top-0 h-16 bg-neutral-50   dark:bg-[#2b3945] shadow-md ">
       <div className="container flex justify-between items-center w-4/5 mx-auto my-0 py-4">
         {/* Main Brand */}
         <Link
           to="/rest-api-countries-react"
           className="font-extrabold text-sm text-[#111517] dark:text-white"
         >
-         <h1> Where in the world?</h1>
+          <h1> Where in the world?</h1>
         </Link>
 
         {/* Switch Theme */}
         <Toggle />
       </div>
-    </div>
+    </header>
   );
 };
 
