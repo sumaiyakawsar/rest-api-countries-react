bd5abe6d3cf3fcca3909d32b89f1273903fdde1d
Sumaiya Kawsar <sumaiya.kawsar693@gmail.com>
2022-09-10 07:33:26 +0300

    Added scroll to top

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,9 @@ import Country from "./components/Countries/Country";
 import { Routes, Route } from "react-router-dom";
 
 import { ThemeProvider } from "./api/themeContext";
-
+import {AiOutlineArrowUp} from "react-icons/ai";
 import Footer from "./components/Footer/Footer";
+import ScrollToTop from "react-scroll-to-top";
 
 function App() {
   return (
@@ -17,10 +18,19 @@ function App() {
         <Header />
 
         <Routes>
-          <Route path="/rest-api-countries-react" element={<Countries />} exact/>
+          <Route
+            path="/rest-api-countries-react"
+            element={<Countries />}
+            exact
+          />
           <Route path="/countries/:countryName" element={<Country />} />
         </Routes>
 
+        <ScrollToTop
+          smooth="true"
+          className="rounded-full text-lg shadow-xl flex justify-center items-center bg-neutral-50 dark:bg-[#202c37] text-[#111517]  dark:text-white"
+          component={<AiOutlineArrowUp />}
+        />
         <Footer />
       </section>
     </ThemeProvider>
--- src/components/Header/Toggle.jsx
+++ src/components/Header/Toggle.jsx
@@ -9,6 +9,7 @@ const Toggle = () => {
     <div>
       {theme === "dark" ? (
         <button
+          title="light" aria-label="light"
           onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
           className="text-white   shadow-none p-2 focus:outline-none text-md outline-none ring-transparent cursor-pointer"
         >
@@ -16,6 +17,7 @@ const Toggle = () => {
         </button>
       ) : (
         <button
+          title="dark" aria-label="dark"
           onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
           className="text-[#111517] focus:outline-none shadow-none p-2 text-md rounded-full outline-none ring-transparent cursor-pointer"
         >
